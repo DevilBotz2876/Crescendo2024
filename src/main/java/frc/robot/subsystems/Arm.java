3c9a2c3e61be13dcf158933049d74718a8c15b65
Ethan Porter <emanporter2009@gmail.com>
2024-01-13 14:27:09 -0500

    added parameter for setAngle

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -1,11 +1,11 @@
 package frc.robot.subsystems;
 
 public interface Arm {
-  // gets the angle of the arm.
-  public default void getAngle() {}
-
-  // sets of the angle of the arm.
-  public default double setAngle() {
+  // gets the angle of the arm
+  public default double getAngle() {
     return 0;
   }
+
+  // sets of the angle of the arm
+  public default void setAngle(double degrees) {}
 }
