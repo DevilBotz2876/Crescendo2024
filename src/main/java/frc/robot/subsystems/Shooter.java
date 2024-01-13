a1224e0e80fe3261bab1ecf06e506e1742fdd62e
Ethan Porter <emanporter2009@gmail.com>
2024-01-13 12:58:49 -0500

    Ran spotlessApply

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
 public interface Shooter {
-    
+
   // Sets enable(true/false) to be used for the motors. on=true and off=false
   public default void setEnabled(boolean isEnabled) {}
 
@@ -9,13 +9,12 @@ public interface Shooter {
   public default boolean getEnabled() {
     return false;
   }
-// Sets the voltage of the motors.
+
+  // Sets the voltage of the motors.
   public default void setVoltage(double voltage) {}
-  
-// gets the voltage of the motors.
-  public default double getVoltage(){
+
+  // gets the voltage of the motors.
+  public default double getVoltage() {
     return 0;
   }
-
-
 }
