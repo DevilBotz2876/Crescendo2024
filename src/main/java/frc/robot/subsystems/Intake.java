75d871a3978b234f756379f80fa3aec97cf77066
Carter <tha.c.bug@gmail.com>
2024-01-13 12:12:57 -0500

    Created Intake interface

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,16 @@
+package frc.robot.subsystems;
+
+public interface Intake {
+    //Sets the Intake to a mode(in/out)
+    public default void setMode(String direction) {}
+    
+    //Gets the Intake mode (in/out)
+    public default String getMode() {return null;}
+    
+    //Sets enable(true/false) to be used for the motors. on=true and off=false
+    public default void setEnabled(boolean isEnabled) {}
+    
+    //Gets enable or disable Intake value.
+    public default boolean getEnabled() {return false;}
+
+}
