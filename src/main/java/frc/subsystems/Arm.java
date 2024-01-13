3979703b42b295df7dc26bd0528f51eb6f525032
Carter <tha.c.bug@gmail.com>
2024-01-13 13:31:19 -0500

    ran spotlessApply

--- src/main/java/frc/subsystems/Arm.java
+++ src/main/java/frc/subsystems/Arm.java
@@ -1,12 +1,11 @@
 package frc.subsystems;
 
 public interface Arm {
-    //gets the angle of the arm.
-    public default void getAngle() {
+  // gets the angle of the arm.
+  public default void getAngle() {}
 
-    }
-    //sets of the angle of the arm.
-    public default double setAngle() {
-        return 0;
-    }
+  // sets of the angle of the arm.
+  public default double setAngle() {
+    return 0;
+  }
 }
