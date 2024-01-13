62420beadd0dfe220ed551a4f2e809b676a8720a
Carter <tha.c.bug@gmail.com>
2024-01-13 13:30:07 -0500

    Created arm interface

---
+++ src/main/java/frc/subsystems/Arm.java
@@ -0,0 +1,12 @@
+package frc.subsystems;
+
+public interface Arm {
+    //gets the angle of the arm.
+    public default void getAngle() {
+
+    }
+    //sets of the angle of the arm.
+    public default double setAngle() {
+        return 0;
+    }
+}
