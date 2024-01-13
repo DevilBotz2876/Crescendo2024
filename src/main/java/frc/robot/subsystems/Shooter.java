b85507fb5f59cd9d32c3e3541254bf20dfd349eb
Ethan Porter <emanporter2009@gmail.com>
2024-01-13 14:15:27 -0500

    renamed 'enable' to 'noteOut'

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -2,13 +2,13 @@ package frc.robot.subsystems;
 
 public interface Shooter {
   // Disable the shooter
-  public default void disable(double voltage) {}
+  public default void disable() {}
 
-  // Enable the shooter.
-  public default void enable(double voltage) {}
+  // Enable the shooter with the .
+  public default void noteOut(double voltage) {}
 
   // Set the voltage of the motors.
-  public default double setVoltage () {
+  public default double setVoltage() {
     return 0;
   }
 }
