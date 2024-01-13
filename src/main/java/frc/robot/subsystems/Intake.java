e37a026a37e5a82c11b427bdfbb55c2155e11159
Ethan Porter <emanporter2009@gmail.com>
2024-01-13 13:49:24 -0500

    Revised methods

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -1,19 +1,12 @@
 package frc.robot.subsystems;
 
 public interface Intake {
-  // Sets the Intake to a mode(in/out)
-  public default void setMode(String direction) {}
+  // Enables motors so it feeds into intake
+  public default void noteIn() {}
 
-  // Gets the Intake mode (in/out)
-  public default String getMode() {
-    return null;
-  }
+  // Enables motors so it feeds out of intake
+  public default void noteOut() {}
 
-  // Sets enable(true/false) to be used for the motors. on=true and off=false
-  public default void setEnabled(boolean isEnabled) {}
-
-  // Gets enable or disable Intake value.
-  public default boolean getEnabled() {
-    return false;
-  }
+  // Disable the motors for the Intake
+  public default void disable() {}
 }
