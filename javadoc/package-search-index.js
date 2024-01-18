7f1a46fbae85fd5a4b7dacf07fa9c1a51d210520
BrownGenius <49494444+BrownGenius@users.noreply.github.com>
2024-01-18 13:51:32 +0000

    Deploying to javadoc from @ DevilBotz2876/Crescendo2024@4f05fdcd137ca2bffc7c7cb18522761431b94f07 🚀

--- javadoc/package-search-index.js
+++ javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.robot"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"frc.robot"},{"l":"frc.robot.subsystems.arm"}];updateSearchResults();
