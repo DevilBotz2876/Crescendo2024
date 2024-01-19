c7a540c2c7123730a74ff4b05f3448489c11a814
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-01-19 14:17:01 -0500

    Basic Swerve YAGSL implementation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,13 +6,32 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.drive.DriveCommand;
+import frc.robot.subsystems.drive.DriveSwerveYAGSL;
 
 public class RobotContainer {
+  CommandXboxController controller = new CommandXboxController(0);
+  DriveSwerveYAGSL drive = new DriveSwerveYAGSL();
+
   public RobotContainer() {
     configureBindings();
   }
 
-  private void configureBindings() {}
+  private void configureBindings() {
+    drive.setDefaultCommand(
+        new DriveCommand(
+            drive,
+            () -> -controller.getLeftY(),
+            () -> -controller.getLeftX(),
+            () -> -controller.getRightX()));
+
+    controller
+        .start()
+        .onTrue(
+            new InstantCommand(() -> drive.setFieldOrientedDrive(!drive.isFieldOrientedDrive())));
+  }
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
