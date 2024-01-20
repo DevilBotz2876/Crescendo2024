dffa504e1a68d3b85cb511137a8a8bffbe4a4377
Ethan Porter <emanporter2009@gmail.com>
2024-01-20 08:51:59 -0500

    added ShooterIOSparkMax

---
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIOSparkMax.java
@@ -0,0 +1,32 @@
+package frc.robot.subsystems.shooter;
+
+import com.revrobotics.CANSparkLowLevel.MotorType;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.util.Units;
+
+public class ShooterIOSparkMax implements ShooterIO {
+  private static final double GEAR_RATIO = 1.5;
+
+  private final CANSparkMax leader = new CANSparkMax(0, MotorType.kBrushless);
+  private final CANSparkMax follower = new CANSparkMax(1, MotorType.kBrushless);
+  private final RelativeEncoder encoder = leader.getEncoder();
+
+  public ShooterIOSparkMax() {
+    leader.setInverted(false);
+    follower.follow(leader, false);
+  }
+
+  @Override
+  public void updateInputs(ShooterIOInputs inputs) {
+    inputs.velocityRadPerSec = 
+        Units.rotationsPerMinuteToRadiansPerSecond(encoder.getVelocity() / GEAR_RATIO);
+    inputs.appliedVolts = leader.getAppliedOutput() * leader.getBusVoltage();
+  }
+
+  @Override
+  public void setVoltage(double volts) {
+    leader.setVoltage(volts);
+    
+  }
+}
