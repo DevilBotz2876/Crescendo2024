f58f702f959dc44699cd6a5fd62a72e2e04c2e68
Ethan Porter <115738405+StewardHail3433@users.noreply.github.com>
2024-01-20 14:24:56 -0500

    Setup intake interface (#6)
    * Created Intake simulation
    
    ---------
    
    Co-authored-by: Carter <tha.c.bug@gmail.com>

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,17 +4,22 @@
 
 package frc.robot;
 
+// import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.IntakeBaseCommand;
 import frc.robot.commands.ShooterEnable;
+import frc.robot.subsystems.intake.IntakeBase;
+import frc.robot.subsystems.intake.IntakeIOSim;
 import frc.robot.subsystems.shooter.ShooterIOSim;
 import frc.robot.subsystems.shooter.ShooterSubsystem;
 
 public class RobotContainer {
   public final CommandXboxController controller;
   public final ShooterSubsystem shooter;
+  public final IntakeBase intake;
 
   public RobotContainer() {
     controller = new CommandXboxController(0);
@@ -24,10 +29,13 @@ public class RobotContainer {
 
     if (swerveBot) {
       shooter = null;
+      intake = null;
     } else if (tankBot) {
       shooter = null;
+      intake = null;
     } else {
       shooter = new ShooterSubsystem(new ShooterIOSim());
+      intake = new IntakeBase(new IntakeIOSim());
     }
 
     configureBindings();
@@ -37,6 +45,11 @@ public class RobotContainer {
     shooter.setDefaultCommand(new InstantCommand(() -> shooter.disable(), shooter));
 
     controller.rightTrigger().whileTrue(new ShooterEnable(shooter));
+    intake.setDefaultCommand(
+        new IntakeBaseCommand(
+            intake,
+            () -> controller.rightBumper().getAsBoolean(),
+            () -> controller.leftBumper().getAsBoolean()));
   }
 
   public Command getAutonomousCommand() {
