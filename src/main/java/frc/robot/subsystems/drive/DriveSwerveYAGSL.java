4486cf675051421d2bb91f57abd13ed00015f579
joshuman <joshuamanoj2008@gmail.com>
2024-01-20 15:33:04 -0500

    Started Implementing PhotonVision code
    There are still many errors.

--- src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
@@ -1,9 +1,18 @@
 package frc.robot.subsystems.drive;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.Kinematics;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
 import java.io.File;
+
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
+import com.pathplanner.lib.util.ReplanningConfig;
+
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
@@ -14,6 +23,14 @@ public class DriveSwerveYAGSL extends DriveBase {
       new File(Filesystem.getDeployDirectory(), "swervePracticeBot");
   private SwerveDrive swerveDrive;
   private boolean fieldOrientedDrive = false;
+      public Pose2d getPose(){
+      return swerveDrive.getPose();
+    }
+    public void resetodometry(){}
+    public ChassisSpeeds getRobotVelocity()
+  {
+    return Kinematics.toChassisSpeeds(getStates());
+  }
 
   public DriveSwerveYAGSL() {
     try {
@@ -21,6 +38,32 @@ public class DriveSwerveYAGSL extends DriveBase {
     } catch (Exception e) {
       throw new RuntimeException(e);
     }
+
+     AutoBuilder.configureHolonomic(
+                this::getPose, // Robot pose supplier
+                this::resetodometry, // Method to reset odometry (will be called if your auto has a starting pose)
+                this::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+                this::driveRobotRelative, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+                new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in your Constants class
+                        new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+                        new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
+                        4.5, // Max module speed, in m/s
+                        0.4, // Drive base radius in meters. Distance from robot center to furthest module.
+                        new ReplanningConfig() // Default path replanning config. See the API for the options here
+                ),
+                () -> {
+                    // Boolean supplier that controls when the path will be mirrored for the red alliance
+                    // This will flip the path being followed to the red side of the field.
+                    // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
+
+                    var alliance = DriverStation.getAlliance();
+                    if (alliance.isPresent()) {
+                        return alliance.get() == DriverStation.Alliance.Red;
+                    }
+                    return false;
+                },
+                this // Reference to this subsystem to set requirements
+        );
   }
 
   @Override
