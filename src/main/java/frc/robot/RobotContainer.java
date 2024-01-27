0c92476afe9adf0b1abed0ff457ac4791f48489f
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-01-27 11:54:57 -0500

    Increased joystick deadband to prevent drift

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -62,9 +62,9 @@ public class RobotContainer {
     drive.setDefaultCommand(
         new DriveCommand(
             drive,
-            () -> MathUtil.applyDeadband(-controller.getLeftY(), 0.01),
-            () -> MathUtil.applyDeadband(-controller.getLeftX(), 0.01),
-            () -> MathUtil.applyDeadband(-controller.getRightX(), 0.01)));
+            () -> MathUtil.applyDeadband(-controller.getLeftY(), 0.05),
+            () -> MathUtil.applyDeadband(-controller.getLeftX(), 0.05),
+            () -> MathUtil.applyDeadband(-controller.getRightX(), 0.05)));
     // TODO: Move deadband to constants file
 
     controller
