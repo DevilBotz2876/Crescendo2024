7aecb2dcf61ffb90572edd8c558a6fcfd36304c8
Ethan Porter <emanporter2009@gmail.com>
2024-02-03 14:31:44 -0500

    leader/follower for motors

--- src/main/java/frc/robot/subsystems/arm/ArmIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/arm/ArmIOSparkMax.java
@@ -8,28 +8,27 @@ import edu.wpi.first.wpilibj.DutyCycleEncoder;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class ArmIOSparkMax implements ArmIO {
+  //Leader
   private final CANSparkMax left = new CANSparkMax(2, MotorType.kBrushless);
 
+  //follower
   private final CANSparkMax right = new CANSparkMax(3, MotorType.kBrushless);
-  ;
+  
   private final DutyCycleEncoder encoder;
 
   private SparkPIDController leftPid = left.getPIDController();
-  private SparkPIDController rightPid = right.getPIDController();
 
   public double lkP, lkI, lkD, lkIz, lkFF, lkMaxOutput, lkMinOutput, lmaxRPS;
-  public double rkP, rkI, rkD, rkIz, rkFF, rkMaxOutput, rkMinOutput, rmaxRPS;
 
   public ArmIOSparkMax() {
     /* TODO: Instantiate 2x SparkMax motors and absolute encoder */
     encoder = new DutyCycleEncoder(0);
     encoder.setPositionOffset(0); // This is place holder
-    encoder.setDutyCycleRange(1, 1024);
-    encoder.setDistancePerRotation(360);
-
-    right.setInverted(false);
+    encoder.setDutyCycleRange(1 / 1025, 1024 / 1025);
+    encoder.setDistancePerRotation(2 * Math.PI);
 
     left.setInverted(false);
+    right.follow(left, false);
 
     left.enableVoltageCompensation(12.0);
     left.setSmartCurrentLimit(30);
@@ -39,7 +38,6 @@ public class ArmIOSparkMax implements ArmIO {
     // TODO: these values are samples picked from REV example PID code.  Need to tune PID and choose
     // real values.
 
-    // left
     lkP = 6e-5;
     lkI = 0;
     lkD = 0;
@@ -47,19 +45,8 @@ public class ArmIOSparkMax implements ArmIO {
     lkFF = 0.000015;
     lkMaxOutput = 1;
     lkMinOutput = -1;
-    rmaxRPS = 300;
+    lmaxRPS = 300;
 
-    // right
-    rkP = 6e-5;
-    rkI = 0;
-    rkD = 0;
-    rkIz = 0;
-    rkFF = 0.000015;
-    rkMaxOutput = 1;
-    rkMinOutput = 0;
-    rmaxRPS = 300;
-
-    // left
     leftPid.setP(lkP);
     leftPid.setI(lkI);
     leftPid.setD(lkD);
@@ -67,15 +54,6 @@ public class ArmIOSparkMax implements ArmIO {
     leftPid.setFF(lkFF);
     leftPid.setOutputRange(lkMinOutput, lkMaxOutput);
 
-    // TODO: probably remove right since Arm will have one motor, not two independent motors
-    rightPid.setP(rkP);
-    rightPid.setI(rkI);
-    rightPid.setD(rkD);
-    rightPid.setIZone(rkIz);
-    rightPid.setFF(rkFF);
-    rightPid.setOutputRange(rkMinOutput, rkMaxOutput);
-
-    // left
     SmartDashboard.putNumber("Arm/left/P Gain", lkP);
     SmartDashboard.putNumber("Arm/left/I Gain", lkI);
     SmartDashboard.putNumber("Arm/left/D Gain", lkD);
@@ -83,16 +61,6 @@ public class ArmIOSparkMax implements ArmIO {
     SmartDashboard.putNumber("Arm/left/Feed Forward", lkFF);
     SmartDashboard.putNumber("Arm/left/Max Output", lkMaxOutput);
     SmartDashboard.putNumber("Arm/left/Min Output", lkMinOutput);
-
-    // TODO: probably remove this since Arm will have one motor, not two independent motors
-    //
-    SmartDashboard.putNumber("Arm/right/P Gain", rkP);
-    SmartDashboard.putNumber("Arm/right/I Gain", rkI);
-    SmartDashboard.putNumber("Arm/right/D Gain", rkD);
-    SmartDashboard.putNumber("Arm/right/I Zone", rkIz);
-    SmartDashboard.putNumber("Arm/right/Feed Forward", rkFF);
-    SmartDashboard.putNumber("Arm/right/Max Output", rkMaxOutput);
-    SmartDashboard.putNumber("Arm/right/Min Output", rkMinOutput);
   }
 
   /** Updates the set of loggable inputs. */
@@ -104,7 +72,6 @@ public class ArmIOSparkMax implements ArmIO {
     inputs.leftAppliedVolts = left.getAppliedOutput() * left.getBusVoltage();
     inputs.rightAppliedVolts = right.getAppliedOutput() * right.getBusVoltage();
 
-    // left
     double lp = SmartDashboard.getNumber("Arm/left/P Gain", 0);
     double li = SmartDashboard.getNumber("Arm/left/I Gain", 0);
     double ld = SmartDashboard.getNumber("Arm/left/D Gain", 0);
@@ -113,15 +80,6 @@ public class ArmIOSparkMax implements ArmIO {
     double lmax = SmartDashboard.getNumber("Arm/left/Max Output", 0);
     double lmin = SmartDashboard.getNumber("Arm/left/Min Output", 0);
 
-    double rp = SmartDashboard.getNumber("Arm/right/P Gain", 0);
-    double ri = SmartDashboard.getNumber("Arm/right/I Gain", 0);
-    double rd = SmartDashboard.getNumber("Arm/right/D Gain", 0);
-    double riz = SmartDashboard.getNumber("Arm/right/I Zone", 0);
-    double rff = SmartDashboard.getNumber("Arm/right/Feed Forward", 0);
-    double rmax = SmartDashboard.getNumber("Arm/right/Max Output", 0);
-    double rmin = SmartDashboard.getNumber("Arm/right/Min Output", 0);
-
-    // left
     if ((lp != lkP)) {
       leftPid.setP(lp);
       lkP = lp;
@@ -147,33 +105,6 @@ public class ArmIOSparkMax implements ArmIO {
       lkMinOutput = lmin;
       lkMaxOutput = lmax;
     }
-
-    // right
-    if ((rp != rkP)) {
-      rightPid.setP(rp);
-      rkP = rp;
-    }
-    if ((ri != rkI)) {
-      rightPid.setI(ri);
-      rkI = ri;
-    }
-    if ((rd != rkD)) {
-      rightPid.setD(rd);
-      rkD = rd;
-    }
-    if ((riz != rkIz)) {
-      rightPid.setIZone(riz);
-      rkIz = riz;
-    }
-    if ((rff != rkFF)) {
-      rightPid.setFF(rff);
-      rkFF = rff;
-    }
-    if ((rmax != rkMaxOutput) || (rmin != rkMinOutput)) {
-      rightPid.setOutputRange(rmin, rmax);
-      rkMinOutput = rmin;
-      rkMaxOutput = rmax;
-    }
   }
 
   @Override
@@ -187,14 +118,6 @@ public class ArmIOSparkMax implements ArmIO {
 
     SmartDashboard.putNumber("Shooter/left/positionRad", radians);
     SmartDashboard.putNumber("Shooter/left/ProcessVariable", encoder.getAbsolutePosition());
-
-    rightPid.setReference(
-        Units.radiansToRotations(radians),
-        CANSparkMax.ControlType.kPosition,
-        0, // Arbitrary slotID, you may need to adjust this based on your configuration
-        ffVolts);
-    SmartDashboard.putNumber("Shooter/right/positionRad", radians);
-    SmartDashboard.putNumber("Shooter/right/ProcessVariable", encoder.getAbsolutePosition());
   }
 
   /** Run the arm motor at the specified voltage. */
@@ -202,6 +125,5 @@ public class ArmIOSparkMax implements ArmIO {
   public void setVoltage(double volts) {
     /* TODO: Implement SparkMax Code Here */
     left.setVoltage(volts);
-    right.setVoltage(volts);
   }
 }
