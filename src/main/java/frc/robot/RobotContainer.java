74eef409041e8e0742ed4c3148167ff7aa9d715f
Ethan Porter <emanporter2009@gmail.com>
2024-02-03 14:38:26 -0500

    added ArmSysIdBindings

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -13,6 +13,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.ArmCommand;
 import frc.robot.commands.IntakeBaseCommand;
 import frc.robot.commands.ShooterEnable;
@@ -106,8 +107,17 @@ public class RobotContainer {
       arm = new ArmSubsystem(new ArmIOStub());
     }
 
-    configureBindings();
+    //configureBindings();
+    ArmSysIdBindings();
   }
+
+  private void ArmSysIdBindings() {
+    controller.a().whileTrue(arm.sysIdQuasistatic(SysIdRoutine.Direction.kForward));
+    controller.b().whileTrue(arm.sysIdQuasistatic(SysIdRoutine.Direction.kReverse));
+    controller.x().whileTrue(arm.sysIdDynamic(SysIdRoutine.Direction.kForward));
+    controller.y().whileTrue(arm.sysIdDynamic(SysIdRoutine.Direction.kReverse));
+  }
+
 
   private void configureBindings() {
     // shooter.setDefaultCommand(new InstantCommand(() -> shooter.disable(), shooter));
