7260f6da2ba0b326f13e2adc1f0fbbaf90d35ef4
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-02-06 11:40:35 -0500

    Added positionRad to captured input

--- src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterIO.java
@@ -5,8 +5,9 @@ import org.littletonrobotics.junction.AutoLog;
 public interface ShooterIO {
   @AutoLog
   public class ShooterIOInputs {
-    public double velocityRadPerSec = 0.0;
+    public double velocityRadPerSec = 0.0;    
     public double appliedVolts = 0.0;
+    public double positionRad = 0.0;
   }
 
   /** Updates the set of loggable inputs. */
