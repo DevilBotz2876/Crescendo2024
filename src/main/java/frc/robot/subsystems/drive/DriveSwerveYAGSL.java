7ce36fac87e20ce94bb092398ba9779665c68bee
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-02-09 11:05:24 -0500

    Revert files

--- src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
@@ -6,27 +6,19 @@ import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.ReplanningConfig;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-// import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Filesystem;
-// import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
-// import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
-// import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import java.io.File;
-// import java.util.Map;
 import org.littletonrobotics.junction.AutoLogOutput;
 import swervelib.SwerveDrive;
 import swervelib.parser.SwerveParser;
 
 public class DriveSwerveYAGSL extends DriveBase {
-  private double maximumSpeed = 4.5; // meters/sec
+  private final double maximumSpeed = 4.5; // meters/sec
   private final File swerveJsonDirectory =
       new File(Filesystem.getDeployDirectory(), "swervePracticeBot");
   private SwerveDrive swerveDrive;
   @AutoLogOutput private boolean fieldOrientedDrive = false;
-
-  // ShuffleboardTab tab;
-  // GenericEntry speedLimiterEntry;
 
   public DriveSwerveYAGSL() {
     try {
@@ -46,7 +38,7 @@ public class DriveSwerveYAGSL extends DriveBase {
             // your Constants class
             new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
             new PIDConstants(5.0, 0.0, 0.0), // Rotation PID constants
-            maximumSpeed, // Max module speed, in m/s
+            4.5, // Max module speed, in m/s
             0.4, // Drive base radius in meters. Distance from robot center to furthest module.
             new ReplanningConfig() // Default path replanning config. See the API for the options
             // here
