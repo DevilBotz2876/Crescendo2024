10e119e57113e5c2b325b7c60ca71d091d14c077
Ethan Porter <emanporter2009@gmail.com>
2024-02-17 10:18:08 -0500

    use Arm Interfrace

--- src/main/java/frc/robot/commands/ArmToPositionDebug.java
+++ src/main/java/frc/robot/commands/ArmToPositionDebug.java
@@ -4,8 +4,11 @@ import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.config.RobotConfig.ArmConstants;
+import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.arm.ArmSubsystem;
 import java.util.Map;
 
@@ -14,14 +17,14 @@ import java.util.Map;
  * debug/test/bring type work.
  */
 public class ArmToPositionDebug extends Command {
-  ArmSubsystem arm;
+  Arm arm;
   ShuffleboardTab tab;
   GenericEntry degreesEntry;
 
-  // double setpoint;
-  // double timeMS;
+  double setpoint;
+  double timeMS;
 
-  public ArmToPositionDebug(ArmSubsystem arm) {
+  public ArmToPositionDebug(Arm arm) {
     this.arm = arm;
 
     tab = Shuffleboard.getTab("Arm");
@@ -34,7 +37,7 @@ public class ArmToPositionDebug extends Command {
                     "min", ArmConstants.minAngleInDegrees, "max", ArmConstants.maxAngleInDegrees))
             .getEntry();
 
-    addRequirements(arm);
+    addRequirements((SubsystemBase)arm);
   }
 
   @Override
@@ -43,26 +46,26 @@ public class ArmToPositionDebug extends Command {
   @Override
   public void execute() {
     double setpoint = degreesEntry.getDouble(1.0);
-    // this.setpoint = setpoint;
+    this.setpoint = setpoint;
     arm.setAngle(setpoint);
   }
 
-  // @Override
-  // public boolean isFinished() {
+   @Override
+  public boolean isFinished() {
 
-  //   if (arm.getAngle() > setpoint - Constants.armErroDegrees && arm.getAngle() < setpoint +
-  // Constants.armErroDegrees) {
-  //     timeMS += 20.0;
-  //     if (timeMS == 1000) {
-  //       SmartDashboard.putBoolean("Arm/ArmToPositionDebug/isFinished", true);
-  //       return true;
-  //     }
-  //   } else {
-  //     timeMS = 0.0;
-  //   }
-  //   SmartDashboard.putBoolean("Arm/ArmToPositionDebug/isFinished", false);
-  //   return false;
-  // }
+    if (arm.getAngle() > setpoint - ArmConstants.pidAngleErrorInDegrees
+        && arm.getAngle() < setpoint + ArmConstants.pidAngleErrorInDegrees) {
+      timeMS += 20.0;
+      if (timeMS == 1000) {
+        SmartDashboard.putBoolean("Arm/ArmToPosition/isFinished", true);
+        return true;
+      }
+    } else {
+      timeMS = 0.0;
+    }
+    SmartDashboard.putBoolean("Arm/ArmToPosition/isFinished", false);
+    return false;
+  }
 
   @Override
   public void end(boolean interrupted) {
