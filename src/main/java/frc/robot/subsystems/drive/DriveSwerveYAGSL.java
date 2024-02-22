4ece9e33d84197ba9a6d6327bb76e8272ee39b9b
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-02-22 15:37:25 -0500

    spotlessApply

--- src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
@@ -40,13 +40,16 @@ public class DriveSwerveYAGSL extends DriveBase {
             ::resetOdometry, // Method to reset odometry (will be called if your auto has a starting
         // pose)
         swerveDrive::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-        swerveDrive::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+        swerveDrive::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE
+        // ChassisSpeeds
         new HolonomicPathFollowerConfig( // HolonomicPathFollowerConfig, this should likely live in
             // your Constants class
             new PIDConstants(0.0020645, 0.0, 0.0), // Translation PID constants
             new PIDConstants(0.01, 0.0, 0.00), // Rotation PID constants
             swerveDrive.getMaximumVelocity(), // Max module speed, in m/s
-            swerveDrive.swerveDriveConfiguration.getDriveBaseRadiusMeters(), // Drive base radius in meters. Distance from robot center to furthest module.
+            swerveDrive.swerveDriveConfiguration
+                .getDriveBaseRadiusMeters(), // Drive base radius in meters. Distance from robot
+            // center to furthest module.
             new ReplanningConfig() // Default path replanning config. See the API for the options
             // here
             ),
