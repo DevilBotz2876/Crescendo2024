62d7f192c0092ac2372f8b48268289d507c534cb
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-02-29 17:03:07 -0500

    Disable swerve debug telemetry

--- src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
+++ src/main/java/frc/robot/subsystems/drive/DriveSwerveYAGSL.java
@@ -19,7 +19,6 @@ import swervelib.SwerveDriveTest;
 import swervelib.parser.PIDFConfig;
 import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
-import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 
 public class DriveSwerveYAGSL extends DriveBase {
   private final File swerveJsonDirectory;
@@ -29,7 +28,7 @@ public class DriveSwerveYAGSL extends DriveBase {
   public DriveSwerveYAGSL(String configPath) {
     swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), configPath);
 
-    SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
+    // SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
     try {
       swerveDrive =
           new SwerveParser(swerveJsonDirectory)
