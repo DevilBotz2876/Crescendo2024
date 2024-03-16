08820096328e7f980750a7536e5dd524d707c6d3
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-03-16 01:29:36 -0400

    Revert non-autonomous related changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,7 +6,9 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.Preferences;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.config.RobotConfig;
 import frc.robot.config.RobotConfigInferno;
 import frc.robot.config.RobotConfigPhoenix;
@@ -15,6 +17,7 @@ import frc.robot.controls.DebugControls;
 import frc.robot.controls.DriverControls;
 import frc.robot.controls.PitControls;
 import frc.robot.controls.SysIdControls;
+import frc.robot.util.DevilBotState;
 
 public class RobotContainer {
   public final RobotConfig robotConfig;
@@ -57,40 +60,25 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    /*
-     if (autoCommand == null) {
-       autoCommand =
-           new SequentialCommandGroup(
-               RobotConfig.climber
-                   .getPrepareClimberForMatchStartCommand()
-                   .onlyIf(() -> DevilBotState.climberNeedsToBeZeroedAtStart), // Move climber down
-               new ParallelCommandGroup(
-                   RobotConfig.climber
-                       .getAutoZeroCommand()
-                       .onlyIf(
-                           () ->
-                               DevilBotState
-                                   .climberNeedsToBeZeroedAtStart), // auto zero climber command
-                   // while
-                   // running selected auto
-                   RobotConfig.autoChooser
-                       .getSelected()
-                       .andThen(
-                           new ParallelCommandGroup(
-                               RobotConfig.shooter.getTurnOffCommand(),
-                               RobotConfig.intake.getTurnOffCommand()))),
-               new InstantCommand(() -> DevilBotState.climberNeedsToBeZeroedAtStart = false),
-               new ParallelCommandGroup(
-                   RobotConfig.shooter.getTurnOffCommand(),
-                   RobotConfig.intake
-                       .getTurnOffCommand())); // Turn off shooter/intake after autonomous
-     }
-     return autoCommand;
-    */
-    return RobotConfig.autoChooser
-        .getSelected()
-        .andThen(
-            new ParallelCommandGroup(
-                RobotConfig.shooter.getTurnOffCommand(), RobotConfig.intake.getTurnOffCommand()));
+    if (autoCommand == null) {
+      autoCommand =
+          new SequentialCommandGroup(
+              RobotConfig.climber
+                  .getPrepareClimberForMatchStartCommand()
+                  .onlyIf(() -> DevilBotState.climberNeedsToBeZeroedAtStart), // Move climber down
+              new ParallelCommandGroup(
+                  RobotConfig.climber
+                      .getAutoZeroCommand()
+                      .onlyIf(
+                          () ->
+                              DevilBotState
+                                  .climberNeedsToBeZeroedAtStart), // auto zero climber command
+                  // while
+                  // running selected auto
+                  RobotConfig.autoChooser.getSelected()),
+              new InstantCommand(() -> DevilBotState.climberNeedsToBeZeroedAtStart = false));
+    }
+
+    return autoCommand;
   }
 }
