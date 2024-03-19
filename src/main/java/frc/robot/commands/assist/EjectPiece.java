fdf5d6a923dd74631f8fc486f446ced64b0fa97c
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-03-18 22:11:30 -0400

    Reverse shooter as part of Eject

--- src/main/java/frc/robot/commands/assist/EjectPiece.java
+++ src/main/java/frc/robot/commands/assist/EjectPiece.java
@@ -1,11 +1,15 @@
 package frc.robot.commands.assist;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import frc.robot.commands.arm.ArmToPositionTP;
+import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.arm.ArmToPosition;
 import frc.robot.commands.intake.IntakeOut;
 import frc.robot.config.RobotConfig;
 import frc.robot.subsystems.arm.Arm;
 import frc.robot.subsystems.intake.Intake;
+import frc.robot.subsystems.shooter.Shooter;
 
 public class EjectPiece extends SequentialCommandGroup {
 
@@ -13,12 +17,20 @@ public class EjectPiece extends SequentialCommandGroup {
   // Run intake in reverse
   // Turn off intake
 
-  public EjectPiece(Intake intake, Arm arm) {
+  public EjectPiece(Intake intake, Arm arm, Shooter shooter) {
 
     double current_pos = arm.getAngle();
     if (current_pos < RobotConfig.ArmConstants.ejectAngleInDegrees) {
-      addCommands(new ArmToPositionTP(() -> RobotConfig.ArmConstants.ejectAngleInDegrees, arm));
+      addCommands(new ArmToPosition(arm, () -> RobotConfig.ArmConstants.ejectAngleInDegrees));
     }
-    addCommands(new IntakeOut(intake).withTimeout(1));
+
+    addCommands(
+        new ParallelCommandGroup(
+                new SequentialCommandGroup(
+                    new InstantCommand(() -> shooter.runVoltage(-12.0)),
+                    new WaitCommand(0.50),
+                    new InstantCommand(() -> shooter.runVoltage(0.0))),
+                new IntakeOut(intake))
+            .withTimeout(1));
   }
 }
