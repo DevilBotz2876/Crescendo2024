3580bad4680381874995d8fb867fc3234a856bf7
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-03-27 13:17:01 -0400

    Updated camera location based on measurements

--- src/main/java/frc/robot/config/RobotConfigInferno.java
+++ src/main/java/frc/robot/config/RobotConfigInferno.java
@@ -131,7 +131,7 @@ public class RobotConfigInferno extends RobotConfig {
             "shooter",
             "1188",
             new Transform3d(
-                new Translation3d(Units.inchesToMeters(10.5), 0, Units.inchesToMeters(13)),
+                new Translation3d(-Units.inchesToMeters(10.5), 0, Units.inchesToMeters(13)),
                 new Rotation3d(0, Units.degreesToRadians(-28), Units.degreesToRadians(180)))));
 
     cameras.add(
