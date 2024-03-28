a92cb9860331986ca895e42ad73a29699edf34b5
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-03-28 19:53:34 -0400

    "Drive to Amp" command (test)

--- src/main/java/frc/robot/controls/DebugControls.java
+++ src/main/java/frc/robot/controls/DebugControls.java
@@ -1,5 +1,12 @@
 package frc.robot.controls;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.GoalEndState;
+import com.pathplanner.lib.path.PathConstraints;
+import com.pathplanner.lib.path.PathPlannerPath;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.GenericEntry;
 import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
@@ -168,6 +175,36 @@ public class DebugControls {
         .withPosition(colIndex, rowIndex++)
         .withSize(2, 1);
 
+    // Create a list of bezier points from poses. Each pose represents one waypoint.
+    // The rotation component of the pose should be the direction of travel. Do not use holonomic
+    // rotation.
+    List<Translation2d> bezierPoints =
+        PathPlannerPath.bezierFromPoses(
+            RobotConfig.drive.getPose(),
+            new Pose2d(1.8, 7.5, Rotation2d.fromDegrees(90)),
+            new Pose2d(1.8, 7.7, Rotation2d.fromDegrees(90)));
+
+    // Create the path using the bezier points created above
+    PathPlannerPath path =
+        new PathPlannerPath(
+            bezierPoints,
+            new PathConstraints(
+                1.0,
+                3.0,
+                2 * Math.PI,
+                4 * Math.PI), // The constraints for this path. If using a differential drivetrain,
+            // the angular constraints have no effect.
+            new GoalEndState(
+                0.0,
+                Rotation2d.fromDegrees(
+                    -90)) // Goal end state. You can set a holonomic rotation here. If using a
+            // differential drivetrain, the rotation will have no effect.
+            );
+
+    Command driveToBlueAmp = AutoBuilder.followPath(path);
+    driveToBlueAmp.setName("Drive To Amp");
+    debugTab.add(driveToBlueAmp).withPosition(colIndex, rowIndex++).withSize(2, 1);
+    ;
     /*
         colIndex += 2;
         rowIndex = 1;
