5514e93fa9c35d1a7af105a4441beb89d00876ed
Nilesh Agarwalla <nilesh@alum.mit.edu>
2024-03-29 18:23:28 -0400

    disabled left/right cameras

--- src/main/java/frc/robot/config/RobotConfigInferno.java
+++ src/main/java/frc/robot/config/RobotConfigInferno.java
@@ -143,6 +143,7 @@ public class RobotConfigInferno extends RobotConfig {
                 new Translation3d(0.3048, 0, 0.22),
                 new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(0)))));
 
+    /*
     cameras.add(
         new VisionCamera(
             "right",
@@ -160,6 +161,7 @@ public class RobotConfigInferno extends RobotConfig {
                 new Translation3d(
                     -Units.inchesToMeters(5), Units.inchesToMeters(12.5), Units.inchesToMeters(5)),
                 new Rotation3d(0, Units.degreesToRadians(-30), Units.degreesToRadians(90)))));
+    */
 
     vision = new VisionSubsystem(cameras, AprilTagFields.k2024Crescendo.loadAprilTagLayoutField());
 
